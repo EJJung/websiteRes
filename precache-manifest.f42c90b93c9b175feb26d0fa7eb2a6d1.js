19a08698ba0b9e9b6a3fc1b94e8fd787caef6493
Euijin Jung <euijjung@umich.edu>
2021-03-18 14:09:33 -0400

    Updates

---
+++ precache-manifest.f42c90b93c9b175feb26d0fa7eb2a6d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65843b02bf1b036e295d7259d5f97846",
+    "url": "/websiteRes/index.html"
+  },
+  {
+    "revision": "09e42106583463cc97ee",
+    "url": "/websiteRes/static/js/2.a626e842.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/websiteRes/static/js/2.a626e842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c184e29d5d9188ca0475",
+    "url": "/websiteRes/static/js/main.a1012c7d.chunk.js"
+  },
+  {
+    "revision": "7575ec730e8031b3b6e9",
+    "url": "/websiteRes/static/js/runtime-main.74629c7b.js"
+  }
+]);
