c3721a6f9ae98f319201a8e2a0f78a6442d90913
Euijin Jung <euijjung@umich.edu>
2021-03-19 11:29:55 -0500

    Updates

---
+++ precache-manifest.62803c5abd87e6c849a4143d24c8f735.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea667b86e7bfafd9771de68fca809bde",
+    "url": "/websiteRes/index.html"
+  },
+  {
+    "revision": "072df8873119d8a2a514",
+    "url": "/websiteRes/static/js/2.f432a21e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/websiteRes/static/js/2.f432a21e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83758e5feba9f63200ad",
+    "url": "/websiteRes/static/js/main.8ff88a7e.chunk.js"
+  },
+  {
+    "revision": "7575ec730e8031b3b6e9",
+    "url": "/websiteRes/static/js/runtime-main.74629c7b.js"
+  },
+  {
+    "revision": "c8ed7f7326159e189538479ca1f407b9",
+    "url": "/websiteRes/static/media/2ndBrain.c8ed7f73.svg"
+  },
+  {
+    "revision": "a3218a643abf336d1649b660d28bdf0d",
+    "url": "/websiteRes/static/media/Yerbba.a3218a64.svg"
+  },
+  {
+    "revision": "c060befcee14efa1811f3e44c914869e",
+    "url": "/websiteRes/static/media/analyticIcon.c060befc.svg"
+  },
+  {
+    "revision": "b1ea3c329aef54def3c92e0892ce032d",
+    "url": "/websiteRes/static/media/beams.b1ea3c32.svg"
+  },
+  {
+    "revision": "17045ee52ffa150b3aee914bc82c5e2a",
+    "url": "/websiteRes/static/media/bxr.17045ee5.svg"
+  },
+  {
+    "revision": "f66fb550ad58b45226b6fef1be58d98c",
+    "url": "/websiteRes/static/media/classifier.f66fb550.svg"
+  },
+  {
+    "revision": "2d99e2345e10efebcc9298098177a1de",
+    "url": "/websiteRes/static/media/codingIcon.2d99e234.svg"
+  },
+  {
+    "revision": "3dde2cd19d208518d775bbdd149b21d4",
+    "url": "/websiteRes/static/media/comingSoon.3dde2cd1.gif"
+  },
+  {
+    "revision": "3dcdf8b802f0c3e694f9e9bd8881ab8d",
+    "url": "/websiteRes/static/media/correlation.3dcdf8b8.svg"
+  },
+  {
+    "revision": "33733c49441b3697bdcd1a1647ff6a41",
+    "url": "/websiteRes/static/media/crowdSourcing.33733c49.svg"
+  },
+  {
+    "revision": "a813f72457a833a9b5f8d8ebb8e38a66",
+    "url": "/websiteRes/static/media/cykParser.a813f724.svg"
+  },
+  {
+    "revision": "d6397dfe30bdad57a6c7618fcab65e0f",
+    "url": "/websiteRes/static/media/decoEJ.d6397dfe.png"
+  },
+  {
+    "revision": "27ea52f5f4998c41ef54ec66a0a6179c",
+    "url": "/websiteRes/static/media/designIcon.27ea52f5.svg"
+  },
+  {
+    "revision": "3399a3a28cbf61c4ae0ee2a40c4b79eb",
+    "url": "/websiteRes/static/media/eyeGuide.3399a3a2.svg"
+  },
+  {
+    "revision": "474fe7167fbb870669792b3d92b8cc2f",
+    "url": "/websiteRes/static/media/healthHub.474fe716.svg"
+  },
+  {
+    "revision": "60a3be434ead340c84d534d74980d4e6",
+    "url": "/websiteRes/static/media/lda.60a3be43.svg"
+  },
+  {
+    "revision": "ae7a0ee780a9e463e2068ebc61612e35",
+    "url": "/websiteRes/static/media/logo.ae7a0ee7.svg"
+  },
+  {
+    "revision": "b97648862ce23232c37e0e9f3b3613f5",
+    "url": "/websiteRes/static/media/ma.b9764886.svg"
+  },
+  {
+    "revision": "1439798194214f5f9f3bca2c48669ab1",
+    "url": "/websiteRes/static/media/photoEJ2.14397981.png"
+  },
+  {
+    "revision": "72863a8de510dff4e2baf9c8ab5e5632",
+    "url": "/websiteRes/static/media/pic_01.72863a8d.jpg"
+  },
+  {
+    "revision": "a4ca12bfbee699f6a239ca9d1808da4a",
+    "url": "/websiteRes/static/media/pic_02.a4ca12bf.jpg"
+  },
+  {
+    "revision": "a99479e13ddfb96c6f56625a083a7b88",
+    "url": "/websiteRes/static/media/pic_03.a99479e1.jpg"
+  },
+  {
+    "revision": "30f6a484a2ecce68194f95d3138d868a",
+    "url": "/websiteRes/static/media/pic_04.30f6a484.jpg"
+  },
+  {
+    "revision": "50782f0ff99182b3c944195c0531b8f0",
+    "url": "/websiteRes/static/media/pic_05.50782f0f.jpg"
+  },
+  {
+    "revision": "f47062248938acb5c0affd98a1c40d9f",
+    "url": "/websiteRes/static/media/pic_06.f4706224.jpg"
+  },
+  {
+    "revision": "2b4866c8cc07a05fa523ee33552d847f",
+    "url": "/websiteRes/static/media/pic_07.2b4866c8.jpg"
+  },
+  {
+    "revision": "84235000675f8d2ebd267f04b8bd0413",
+    "url": "/websiteRes/static/media/pic_08.84235000.JPG"
+  },
+  {
+    "revision": "38ca838d7e7c8cab898962d962bde173",
+    "url": "/websiteRes/static/media/pic_09.38ca838d.jpg"
+  },
+  {
+    "revision": "d47c0b293bab2cff323e66e9ccf39a7c",
+    "url": "/websiteRes/static/media/pic_10.d47c0b29.jpg"
+  },
+  {
+    "revision": "18921105bb7c2f55aef7a0b568e64b8a",
+    "url": "/websiteRes/static/media/pic_11.18921105.JPG"
+  },
+  {
+    "revision": "4c224ab4c8cb842cccd0e5c63e6d4c27",
+    "url": "/websiteRes/static/media/pic_12.4c224ab4.jpg"
+  },
+  {
+    "revision": "d06049507dc3a6d685a550052e26cb4a",
+    "url": "/websiteRes/static/media/pic_13.d0604950.jpg"
+  },
+  {
+    "revision": "e7abf0f42e156a37c3b0565fbd46d372",
+    "url": "/websiteRes/static/media/pic_14.e7abf0f4.JPG"
+  },
+  {
+    "revision": "de67f27bc706be4d01630eb9983b1ade",
+    "url": "/websiteRes/static/media/pic_15.de67f27b.jpg"
+  },
+  {
+    "revision": "3e7dd8213a5a633e5434df156471efb1",
+    "url": "/websiteRes/static/media/pic_16.3e7dd821.jpg"
+  },
+  {
+    "revision": "f1e54f46cedae4ce7e2ef440d04551cb",
+    "url": "/websiteRes/static/media/pic_17.f1e54f46.jpg"
+  },
+  {
+    "revision": "e94d5ef5134c1e373702b9b75e6a8092",
+    "url": "/websiteRes/static/media/pic_18.e94d5ef5.jpg"
+  },
+  {
+    "revision": "22125db5ccd0a7ad1acc4f1a736766b1",
+    "url": "/websiteRes/static/media/pic_19.22125db5.jpg"
+  },
+  {
+    "revision": "3758498c7faa2637844fd81433c5ff55",
+    "url": "/websiteRes/static/media/pic_20.3758498c.jpg"
+  },
+  {
+    "revision": "3716d0e17ea1908492ab0598413a68ee",
+    "url": "/websiteRes/static/media/shahid.3716d0e1.svg"
+  },
+  {
+    "revision": "ba1d097882b2c50a1a2132b671780769",
+    "url": "/websiteRes/static/media/upLift.ba1d0978.svg"
+  },
+  {
+    "revision": "dee20123bff29eecba7aa742f828f954",
+    "url": "/websiteRes/static/media/wordembedding.dee20123.svg"
+  },
+  {
+    "revision": "ff7c6c68f9694ddf740deb0e9acd712b",
+    "url": "/websiteRes/static/media/yerbba.ff7c6c68.png"
+  }
+]);
